6050125e19479cb9b5e017e1023ec79180de1881
Valeria Ochoa <dvale14toledo@gmail.com>
2016-12-21 13:17:23 -0500

    Ejercicios en clase

---
+++ import turtle.py
@@ -0,0 +1,49 @@
+import turtle
+##import Screen
+##import RawTurtle
+##screen=Screen()
+##turtle=RawTurtle(screen)
+t=turtle.Pen()
+#t.forward(50)
+#t.left(90)
+#t.forward(50)
+#t.left(90)
+#t.forward(50)
+#t.left(90)
+#t.forward(50)
+##estrella
+##t.reset()
+##a=int(input("numero: "))
+##an=180/a
+##an2=180-an
+##t.reset()
+##for x in range(a):
+##	t.forward(200)
+##	t.left(an2)
+##cuadrado
+##def micuadrado(size):
+##    for x in range(1,5):
+##        t.forward(size)
+##        t.left(90)
+##
+##micuadrado(200)
+##letra
+t.dot(5,"blue")
+t.lt(135)
+t.fd(150)
+t.dot(5,"blue")
+t.right(100)
+t.forward(35)
+t.dot(5,"blue")
+t.right(80)
+t.forward(120)
+t.dot(5,"blue")
+t.lt(110)
+t.forward(100)
+t.dot(5,"blue")
+t.right(80)
+t.forward(35)
+t.dot(5,"blue")
+t.right(105)
+t.fd(150)
+
